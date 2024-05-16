6e0682fa005a618ac80b87e2378b7bda8533b0d2
Chathurika Gajanayaka <chathurikagajanayaka@chathurikas-air.home>
2024-05-16 14:43:00 +0200

    Posts List, get by id, delete and udpate without the media content

--- posts/models.py
+++ posts/models.py
@@ -6,7 +6,7 @@ class Post(models.Model):
     """
     Represents a blog post.
     """
-    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
--- posts/serializers.py
+++ posts/serializers.py
@@ -5,6 +5,8 @@ from .models import Post
 class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
 
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -13,7 +15,6 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = [
-            'id', 'owner', 'created_at', 'updated_at', 'is_published',
-            'title', 'sub_title', 'slug', 'banner', 'body', 'gmap_location_tag',
-            'is_owner'
+            'id', 'owner', 'is_owner', 'profile_id', 'profile_image', 'created_at', 'updated_at', 'is_published',
+            'title', 'sub_title', 'slug', 'banner', 'body', 'gmap_location_tag'
         ]
