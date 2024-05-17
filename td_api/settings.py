ecda6d4c5ec1fc3f7df69c73d2f4e15ebd6130cb
Chathurika Gajanayaka <chathurikagajanayaka@chathurikas-air.home>
2024-05-17 06:59:16 +0200

    more changes

--- td_api/settings.py
+++ td_api/settings.py
@@ -50,7 +50,7 @@ JWT_AUTH_REFRESH_COOKIE = 'td-refresh-token'
 JWT_AUTH_SAMESITE = 'None'
 
 REST_AUTH_SERIALIZERS = {
-    'USER_DETAILS_SERIALIZER': 'drf_api.serializers.AuthUserSerializer'
+    'USER_DETAILS_SERIALIZER': 'td_api.serializers.AuthUserSerializer'
 }
 
 # Quick-start development settings - unsuitable for production
