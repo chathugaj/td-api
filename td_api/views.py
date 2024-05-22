da6fc036103e391fc6c960341aa64ecf7fa9d90f
Chathurika Gajanayaka <chathurikagajanayaka@chathurikas-air.home>
2024-05-22 11:22:53 +0200

    Changed for login logout testing

--- td_api/views.py
+++ td_api/views.py
@@ -11,25 +11,25 @@ def root_route(request):
         "message": "Welcome to the travler's diary API"
     })
 
-@api_view(['POST'])
-def logout_route(request):
-    response = Response()
-    response.set_cookie(
-        key=REST_AUTH.JWT_AUTH_COOKIE,
-        value='',
-        httponly=True,
-        expires='Thu, 01 Jan 1970 00:00:00 GMT',
-        max_age=0,
-        samesite=REST_AUTH.JWT_AUTH_SAMESITE,
-        secure=REST_AUTH.JWT_AUTH_SECURE,
-    )
-    response.set_cookie(
-        key=REST_AUTH.JWT_AUTH_REFRESH_COOKIE,
-        value='',
-        httponly=True,
-        expires='Thu, 01 Jan 1970 00:00:00 GMT',
-        max_age=0,
-        samesite=REST_AUTH.JWT_AUTH_SAMESITE,
-        secure=REST_AUTH.JWT_AUTH_SECURE,
-    )
-    return response
+# @api_view(['POST'])
+# def logout_route(request):
+#     response = Response()
+#     response.set_cookie(
+#         key=REST_AUTH.JWT_AUTH_COOKIE,
+#         value='',
+#         httponly=True,
+#         expires='Thu, 01 Jan 1970 00:00:00 GMT',
+#         max_age=0,
+#         samesite=REST_AUTH.JWT_AUTH_SAMESITE,
+#         secure=REST_AUTH.JWT_AUTH_SECURE,
+#     )
+#     response.set_cookie(
+#         key=REST_AUTH.JWT_AUTH_REFRESH_COOKIE,
+#         value='',
+#         httponly=True,
+#         expires='Thu, 01 Jan 1970 00:00:00 GMT',
+#         max_age=0,
+#         samesite=REST_AUTH.JWT_AUTH_SAMESITE,
+#         secure=REST_AUTH.JWT_AUTH_SECURE,
+#     )
+#     return response
