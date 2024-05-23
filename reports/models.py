c15fc4ab74f60dc09f52c5dd5392f04444c170f8
Chathurika Gajanayaka <chathurikagajanayaka@chathurikas-air.home>
2024-05-24 01:44:22 +0200

    Minor changes related Like api and other minor fixes

--- reports/models.py
+++ reports/models.py
@@ -9,8 +9,8 @@ class Report(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    reason = models.CharField(max_length=150, blank=True)
-    message = models.TextField(max_length=1000, blank=True)
+    reason = models.CharField(max_length=150)
+    message = models.TextField(max_length=1000)
 
     class Meta:
         ordering = ['-created_at']
