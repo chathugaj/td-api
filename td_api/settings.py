88f483b8c0f1df0949fe69864b800e992c2b4a19
Chathurika Gajanayaka <chathurikagajanayaka@chathurikas-air.home>
2024-07-01 08:36:00 +0200

    Slight change to the settings to see if the api requests succeed with different settings

--- td_api/settings.py
+++ td_api/settings.py
@@ -32,7 +32,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 2,
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
+        'dj_rest_auth.jwt_auth.JWTAuthentication',
+        'dj_rest_auth.jwt_auth.JWTCookieAuthentication',
     ),
 }
 if 'DEV' not in os.environ:
