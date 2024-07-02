4d9530fe322813525995a94a3e70d3d11ce93e82
Chathurika Gajanayaka <chathurikagajanayaka@chathurikas-air.home>
2024-07-02 10:01:51 +0200

    revert latest

--- td_api/settings.py
+++ td_api/settings.py
@@ -32,7 +32,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 2,
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'dj_rest_auth.jwt_auth.JWTAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
         'dj_rest_auth.jwt_auth.JWTCookieAuthentication',
     ),
 }
