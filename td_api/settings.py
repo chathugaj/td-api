0576957b3e3d902f7c29a29d0ed050d7250a36d4
Chathurika Gajanayaka <chathurikagajanayaka@chathurikas-air.home>
2024-07-04 07:55:46 +0200

    settings file reverted to a previous version

--- td_api/settings.py
+++ td_api/settings.py
@@ -129,11 +129,11 @@ INSTALLED_APPS = [
 SITE_ID = 1
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
